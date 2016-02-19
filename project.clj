cdd099be8263c03e08ef5e4fcae68cc69ba8cbaa
Michael Pendergrass <mdpendergrass@gmail.com>
2016-02-19 09:40:33 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,20 @@
-(defproject kosmos-nrepl "0.0.1-SNAPSHOT"
+(defproject kosmos/kosmos-nrepl "0.0.1-SNAPSHOT"
+
   :description "simple nrepl server component"
-  :url ""
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]])
+
+  :url "https://bitbucket.org/pupcus/kosmos-web"
+
+  :scm {:url "git@bitbucket.org:bitbucket/kosmos-web"}
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+
+  :deploy-repositories [["snapshots"
+                         {:url "https://clojars.org/repo"
+                          :creds false}]
+                        ["releases"
+                         {:url "https://clojars.org/repo"
+                          :creds false}]])
+

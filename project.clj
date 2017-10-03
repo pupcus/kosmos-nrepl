bdc9940f55dff6a2727c21d01f997502ea5bda75
Michael Pendergrass <mdpendergrass@gmail.com>
2017-10-02 20:25:35 -0400

    try to use cider-nrepl and refactor-nrepl middleware in handler

--- project.clj
+++ project.clj
@@ -10,11 +10,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[kosmos "0.0.6"]
-                 [org.clojure/tools.nrepl "0.2.12"]]
+                 [org.clojure/tools.nrepl "0.2.13"]]
 
-  :profiles {:dev  {:resource-paths ["dev-resources"]
-                    :dependencies [[org.clojure/clojure "1.8.0"]
-                                   [org.slf4j/slf4j-log4j12 "1.7.5"]]}}
+  :profiles {:dev {:resource-paths ["dev-resources"]
+                   :dependencies [[cider/cider-nrepl "0.15.1"]
+                                  [refactor-nrepl "2.3.1"]
+                                  [org.clojure/clojure "1.8.0"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.5"]]}}
 
   :deploy-repositories [["snapshots"
                          {:url "https://clojars.org/repo"
--- src/kosmos/nrepl.clj
+++ src/kosmos/nrepl.clj
@@ -1,8 +1,22 @@
 (ns kosmos.nrepl
   (:require [clojure.tools.logging :as log]
             [clojure.tools.nrepl.server :as nrepl]
-            [com.stuartsierra.component :as component])
+            [com.stuartsierra.component :as component]
+            [kosmos.util :as u])
+
   (:import java.net.ServerSocket))
+
+(defn build-cider-refactor-handler [handler]
+  (when handler
+    (try
+      (require '[refactor-nrepl.middleware])
+      (apply nrepl/default-handler (cons (u/resolve-symbol :refactor-nrepl.middleware/wrap-refactor)
+                                         (map resolve (deref (u/resolve-symbol :cider.nrepl/cider-middleware)))))
+      (catch Exception e
+        (log/debug e)
+        (log/info "[refactor-nrepl.middleware] dependency not found")
+        (comment "ignore: it didn't work")))))
+
 
 (defrecord NreplComponent [port]
   component/Lifecycle
@@ -11,8 +25,13 @@
     (let [nrepl-conf   (merge {:bind "127.0.0.1"}
                               (try
                                 (require '[cider.nrepl])
-                                {:handler (resolve (symbol "cider.nrepl" "cider-nrepl-handler"))}
+                                (let [nrepl-handler (resolve (symbol "cider.nrepl" "cider-nrepl-handler"))
+                                      handler-fn (or (build-cider-refactor-handler nrepl-handler)
+                                                     nrepl-handler)]
+                                  {:handler handler-fn})
                                 (catch Exception e
+                                  (log/debug e)
+                                  (log/info "[cider.nrepl] dependency not found")
                                   {})))
           start-server (fn [conf]
                          (->> nrepl-conf

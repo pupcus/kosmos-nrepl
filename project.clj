7a6d94ca8783d6d4414da7055924ad5577d31bef
Michael Pendergrass <mdpendergrass@gmail.com>
2017-10-29 10:12:37 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -9,14 +9,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[kosmos "0.0.6"]
+  :dependencies [[kosmos "0.0.7"]
                  [org.clojure/tools.nrepl "0.2.13"]]
 
   :profiles {:dev {:resource-paths ["dev-resources"]
                    :dependencies [[cider/cider-nrepl "0.15.1"]
                                   [refactor-nrepl "2.3.1"]
                                   [org.clojure/clojure "1.8.0"]
-                                  [org.slf4j/slf4j-log4j12 "1.7.5"]]}}
+                                  [org.slf4j/slf4j-log4j12 "1.7.25"]]}}
 
   :deploy-repositories [["snapshots"
                          {:url "https://clojars.org/repo"
@@ -34,5 +34,8 @@
                   ["deploy"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
-                  ["vcs" "push"]])
+                  ["vcs" "push"]]
+
+  :global-vars {*warn-on-reflection* true
+                *assert* false})
 
